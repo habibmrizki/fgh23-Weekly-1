f9dfbe1f4a8bf18597ce447523e3575ca0a05cc6
Habib Muhammad Rizki <habibboboho@gmail.com>
2024-11-29 15:35:03 +0700

    Add task1

---
+++ task1.js
@@ -0,0 +1,15 @@
+const product = {
+  name: "Kemeja",
+  rating: 4,
+  price: 135000,
+  isActive: true,
+  images: [
+    "https://s2.bukalapak.com/img/2389632821/w-1000/kemeja_tangan_panjang_slimfit_warna_biru_tua_biru_navy_OT_ke.jpg",
+  ],
+  description: "kemeja pria lengan panjang slimfit kemeja kerja casual tbb39",
+  variants: {
+    names: "L",
+    options: ["Hitam"],
+  },
+  storeName: "Sukses Jaya",
+};
